2e01cdff9d72c6be07bc9dda13322400126efbf8
JoeyBling <2434387555@qq.com>
2020-01-20 14:46:54 +0800

    deploy at 2020-01-20 14:46:54

--- themes/yilia-plus/scripts/helpers/yilia-plus-vendors.js
+++
@@ -1,9 +0,0 @@
-/* global hexo */
-
-'use strict';
-
-/** 自定义链接生成 */
-hexo.extend.helper.register('yilia_plus_vendors', function (url) {
-  if (url.startsWith('//')) return url;
-  return this.url_for(`${url}`);
-});
